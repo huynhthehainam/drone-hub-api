3aa8ce5b9de3328b33986988ae4c15dad002fca6
huynhthehainam <huynhthehainam@gmail.com>
2021-11-19 10:19:52 +0700

    as

--- db.Dockerfile
+++ db.Dockerfile
@@ -3,4 +3,4 @@ FROM postgres:12
 RUN apt update -y
 RUN apt install postgis postgresql-12-postgis-3 -y
 
-# COPY ./init_db/*.sql /docker-entrypoint-initdb.d/
+COPY ./init_db/*.sql /docker-entrypoint-initdb.d/
---
+++ init_db/init_db.sql
@@ -0,0 +1,23 @@
+
+-- Enable PostGIS (as of 3.0 contains just geometry/geography)
+CREATE EXTENSION postgis;
+-- enable raster support (for 3+)
+CREATE EXTENSION postgis_raster;
+-- Enable Topology
+CREATE EXTENSION postgis_topology;
+-- sfcgal not available with all distributions
+-- and other geoprocessing algorithms
+CREATE EXTENSION postgis_sfcgal;
+-- fuzzy matching needed for Tiger
+CREATE EXTENSION fuzzystrmatch;
+-- rule based standardizer
+CREATE EXTENSION address_standardizer;
+-- example rule data set
+CREATE EXTENSION address_standardizer_data_us;
+-- Enable US Tiger Geocoder
+CREATE EXTENSION postgis_tiger_geocoder;
+
+ALTER SYSTEM SET postgis.enable_outdb_rasters TO True;
+ALTER SYSTEM SET postgis.gdal_enabled_drivers TO 'GTiff';
+
+
