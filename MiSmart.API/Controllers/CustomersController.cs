2830afb6cd3181796e446ecb9222fde46c06c6a7
huynhthehainam <huynhthehainam@gmail.com>
2022-01-17 16:35:36 +0700

    update

--- MiSmart.API/Controllers/CustomersController.cs
+++ MiSmart.API/Controllers/CustomersController.cs
@@ -118,6 +118,22 @@ namespace MiSmart.API.Controllers
 
             return response.ToIActionResult();
         }
+        [HttpGet("{id:int}/Devices")]
+        [HasPermission(typeof(AdminPermission))]
+        public IActionResult GetCustomerDevices([FromServices] DeviceRepository deviceRepository, [FromServices] CustomerRepository customerRepository, [FromQuery] PageCommand pageCommand, [FromRoute] Int32 id)
+        {
+            var response = actionResponseFactory.CreateInstance();
+            var customer = customerRepository.Get(ww => ww.ID == id);
+            if (customer is null)
+            {
+                response.AddNotFoundErr("Customer");
+            }
+
+            var listResponse = deviceRepository.GetListResponseView<SmallDeviceViewModel>(pageCommand, ww => ww.CustomerID == customer.ID);
+            listResponse.SetResponse(response);
+
+            return response.ToIActionResult();
+        }
         [HttpDelete("{id:int}")]
         [HasPermission(typeof(AdminPermission))]
         public IActionResult GetCustomerUsers([FromServices] CustomerUserRepository customerUserRepository, [FromQuery] PageCommand pageCommand, [FromRoute] Int32 id)
