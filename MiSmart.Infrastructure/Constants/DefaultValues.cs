e88684e3542980f6fceff70f9fc1286e6caa4a5c
huynhthehainam <huynhthehainam@gmail.com>
2022-02-18 12:46:35 +0700

    remove infrastructure

--- MiSmart.Infrastructure/Constants/DefaultValues.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace MiSmart.Infrastructure.Constants
-{
-    public class DefaultValues
-    {
-        public const String AvatarUrl = "default/images/default_avatar.jpg";
-    }
-}
