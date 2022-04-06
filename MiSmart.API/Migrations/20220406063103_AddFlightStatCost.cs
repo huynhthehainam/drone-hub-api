c323e67d45204d2367f6a6e4a1452b2e240ebefd
huynhthehainam <huynhthehainam@gmail.com>
2022-04-06 13:31:23 +0700

    update

---
+++ MiSmart.API/Migrations/20220406063103_AddFlightStatCost.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MiSmart.API.Migrations
+{
+    public partial class AddFlightStatCost : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "Cost",
+                table: "FlightStats",
+                type: "double precision",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Cost",
+                table: "FlightStats");
+        }
+    }
+}
