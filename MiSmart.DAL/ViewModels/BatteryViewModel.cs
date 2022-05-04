ece2ce640636328d6394b362e3a351d17e4d61f1
huynhthehainam <huynhthehainam@gmail.com>
2022-05-04 10:50:28 +0700

    update battery view model

--- MiSmart.DAL/ViewModels/BatteryViewModel.cs
+++ MiSmart.DAL/ViewModels/BatteryViewModel.cs
@@ -11,13 +11,20 @@ namespace MiSmart.DAL.ViewModels
         public Int32 ID { get; set; }
         public String ActualID { get; set; }
         public Int32? ExecutionCompanyID { get; set; }
+        public String ExecutionCompanyName { get; set; }
+        public String BatteryModel { get; set; }
+        public Int32 BatteryModelID { get; set; }
+        public String BatteryModelName { get; set; }
         public BatteryGroupLogViewModel LastGroup { get; set; }
         public void LoadFrom(Battery entity)
         {
             ID = entity.ID;
             ActualID = entity.ActualID;
             ExecutionCompanyID = entity.ExecutionCompanyID;
+            ExecutionCompanyName = entity.ExecutionCompany?.Name;
             LastGroup = entity.LastGroup is null ? null : ViewModelHelpers.ConvertToViewModel<BatteryGroupLog, BatteryGroupLogViewModel>(entity.LastGroup);
+            BatteryModelID = entity.BatteryModelID;
+            BatteryModelName = entity.BatteryModel.Name;
         }
     }
     public class SmallBatteryModelViewModel : IViewModel<BatteryModel>
