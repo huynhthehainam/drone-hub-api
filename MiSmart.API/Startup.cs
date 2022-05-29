ba72c8fd1dd26c14f233314a3dd154a5c54b1e4f
huynhthehainam <huynhthehainam@gmail.com>
2022-05-29 10:49:11 +0700

    update dotnet

--- MiSmart.API/Startup.cs
+++ MiSmart.API/Startup.cs
@@ -59,7 +59,7 @@ namespace MiSmart.API
 
             services.AddMvc().AddJsonOptions(options =>
             {
-                options.JsonSerializerOptions.IgnoreNullValues = true;
+                options.JsonSerializerOptions.DefaultIgnoreCondition = System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingNull;
             });
             services.AddSwagger(Configuration, new OpenApiInfo
             {
