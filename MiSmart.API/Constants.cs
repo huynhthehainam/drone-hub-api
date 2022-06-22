b1612d2eddf6f36a3c042420f7f9b9b59c569ebe
huynhthehainam@gmail <huynhthehainam@gmail.com>
2022-06-22 11:30:25 +0700

    update

--- MiSmart.API/Constants.cs
+++ MiSmart.API/Constants.cs
@@ -8,6 +8,6 @@ namespace MiSmart.API
     {
         public static List<String> AllowedVersions = new List<String> { "1.2.2", "1.2.3", "1.3.0", "1.3.1", "1.3.2" };
 
-        public static List<String> LowBatteryReportEmailTargets = new List<String> { "huynhthehainam@gmail.com", "namhth@mismart.ai", "viethuy@mismart.ai", "nguyentaimapa1994@gmail.com" };
+        public static List<String> LowBatteryReportEmailTargets = new List<String> { "huynhthehainam@gmail.com", "namhth@mismart.ai", "viethuy@mismart.ai", "nguyentaimapa1994@gmail.com", "kien.mismart@gmail.com" };
     }
 }
