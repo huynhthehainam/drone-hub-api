d8e1c3062aff5f8dd15aea57aeee2f7edf8c4e84
huynhthehainam@gmail <huynhthehainam@gmail.com>
2022-08-17 13:31:09 +0700

    upgrade version

--- MiSmart.API/Constants.cs
+++ MiSmart.API/Constants.cs
@@ -6,6 +6,6 @@ namespace MiSmart.API
 {
     public static class Constants
     {
-        public static List<String> AllowedVersions = new List<String> { "1.2.2", "1.2.3", "1.3.0", "1.3.1", "1.3.2", "1.3.3", "2.0.1" };
+        public static List<String> AllowedVersions = new List<String> { "1.2.2", "1.2.3", "1.3.0", "1.3.1", "1.3.2", "1.3.3", "1.3.4", "2.0.1" };
     }
 }
