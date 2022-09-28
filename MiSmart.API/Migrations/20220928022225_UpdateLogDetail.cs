47aef7dc158d2c91d10a96c4daafbab33a834069
dttrung.work4901 <dttrung.work4901@gmail.com>
2022-09-28 09:25:02 +0700

    update

---
+++ MiSmart.API/Migrations/20220928022225_UpdateLogDetail.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MiSmart.API.Migrations
+{
+    public partial class UpdateLogDetail : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Heigh",
+                table: "LogDetails",
+                newName: "Height");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Height",
+                table: "LogDetails",
+                newName: "Heigh");
+        }
+    }
+}
