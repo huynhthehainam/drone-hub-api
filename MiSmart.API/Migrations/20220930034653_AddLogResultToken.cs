c9cd454ace5e1e38daa57620c5f508c3fd62ae80
huynhthehainam@gmail <huynhthehainam@gmail.com>
2022-09-30 10:47:08 +0700

    update

---
+++ MiSmart.API/Migrations/20220930034653_AddLogResultToken.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MiSmart.API.Migrations
+{
+    public partial class AddLogResultToken : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Token",
+                table: "LogReportResults",
+                type: "text",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Token",
+                table: "LogReportResults");
+        }
+    }
+}
