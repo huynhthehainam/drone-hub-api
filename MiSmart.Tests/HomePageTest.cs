ffd4165301400640a1f1ed6a6d1f4a67b6436eeb
huynhthehainam@gmail <huynhthehainam@gmail.com>
2022-11-11 11:29:09 +0700

    add git precheck

--- MiSmart.Tests/HomePageTest.cs
+++ MiSmart.Tests/HomePageTest.cs
@@ -32,7 +32,7 @@ public class HomePageTest : IClassFixture<CustomWebApplicationFactory<MiSmart.AP
 
 
         // Assert
-        Assert.Empty(allowedVersions);
+        Assert.NotEmpty(allowedVersions);
         // Assert.Equal(HttpStatusCode.OK, homePage.StatusCode);
 
     }
